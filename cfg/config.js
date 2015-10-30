4dfce4ee2a202d77e8b2260cba8736c1dee465bc
Phil Charles <estofi.uk@gmail.com>
2015-10-30 16:32:00 +0000

    Initial commit

---
+++ cfg/config.js
@@ -0,0 +1,14 @@
+if (typeof HML == 'undefined') {
+ HML = {};
+}
+
+HML.Config = {
+ Modifications  : {
+  Carbamidomethyl : {
+                      Carbon  : 2,
+                      Hydrogen: 3,
+                      Nitrogen: 1,
+                      Oxygen  : 1
+                    }
+ }
+}
