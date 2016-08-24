0671c05593658fbddf6e96aefb5b25561e273bdb
Phil Charles <estofi.uk@gmail.com>
2016-08-24 18:53:23 +0100

    Major rework. Move processing of each raw file to a separate WebWorker (MsDataFileWorker) which passes individual QuantTasks to a further pool of WebWorkers (QuantitationWorker).  Significant refacoring of Interface to support this.  Also try to simplify UI control flow (each DOM click event just checks if the element is enabled, and enabling/disabling is handled explicity at the end of each processing section).  Various optimisations to try to keep memory usage down (large data structures changed to arrays rather than objects where possible).  Update code to reflect updates to MSLIB.  Update config and modification files, add explanatory comments to config options.  Config options prefixed by underscore are now not shown in Settings dialog.  Peptide result export re-written and re-enabled (Protein export still disabled for now).

--- cfg/config.js
+++ cfg/config.js
@@ -1,15 +1,15 @@
 if (typeof HML == 'undefined') HML = {};
 
 HML.Config = {
- FixedMods : "Carbamidomethyl (C)",
- PpmError : 10,
- RTSearchWindow  : 1,
- RTMaxShift  : 0.1,
- RTOrder  : -1,
- FindMaxIntensityByLowestIsotopologue  : 1,
- ConsiderIsotopologuesInTop : 0.8,
- IncorporationWindowMin : 10,
- IncorporationWindowMax : 100,
- MaxThreads : 100
+ FixedMods : "Carbamidomethyl (C)", //assume these modifications are on all matching residues (mods are defined in Modifications.js) 
+ PpmError : 10, //lowering this can increase performance (and quant quality if data is high resolution and good mass accuracy)
+ RTSearchWindow  : 1, //lowering this increases performance but may result in missing the peak if chromatographic reproducibility is low
+ RTMaxShift  : 0.1, //max distance between light and heavy peak apex
+ RTOrder  : -1, //should the heavy peak elute before (-1) or after (1) the light peak.  0 is no constraint
+ IncorporationWindowMin : 10, //lowest % incorporation to look for
+ IncorporationWindowMax : 100, //highest % incorporation to look for
+ _FindMaxIntensityByLowestIsotopologue : true, //locate peak apex using the lowest intensity isotopologue in each scan to calculate the intensity of the full distribution (makes algorithm more robust vs co-elution)
+ _ConsiderIsotopologuesInTop : 0.8, //what proportion of the distribution to consider when locating peak apexes (setting to 1 will include low proportion isotopologues and will in most cases increase noise & decrease quant quality) 
+ _QuantitationThreads : 8, //best set to the number of cores (allowing for hyperthreading).  Lower numbers underutilise resources and higher numbers will waste time with context switching.
+ _RawFileMaxBlockProcessSize : Math.pow(2,27), //String length limit is (2^28)-1 in Firefox and (2^28)-(2^4) in Chrome.  N.B. Safari has a limit of (2^31)-1 but Safari is not a currently targeted browser.  If processing Thermo Raw Files (and therefore using ArrayBuffers not strings) this is potentially much larger.
 }
-
--- cfg/modifications.js
+++ cfg/modifications.js
@@ -5,9 +5,14 @@ HML.Modifications = {
                       Carbon  : 2,
                       Hydrogen: 3,
                       Nitrogen: 1,
-                      Oxygen  : 1,
+                      Oxygen  : 1
                     },
   Oxidation       : {
-                      Oxygen  : 1,
+                      Oxygen  : 1
+                    },
+  Acetyl          : {
+                      Carbon  : 2,
+                      Hydrogen: 2,
+                      Oxygen  : 1
                     }
 }
