5c2085fd67826e9a6e8066f41f5d8f36739ed9ba
Phil Charles <estofi.uk@gmail.com>
2017-01-27 18:05:52 +0000

    Further rework.  Update to reflect changes in MSLIB (particularly state change calls and MSLIBWorker refactor).  Throw Errors instead of printing to console.  Add better handling of Config options to Interface; display more progress information during processing; refactor raw/ID file loading; reduce UI update speed; separate out QuantitationWorker task queue.  Update Plot to allow option of log or linear scaling on Chromatogram y-axis and allow precision of y-axis labels to be specified;  fix bug in Chromatogram x-axis range/display length.  Update both MsDataFileWorker and QuantitationWorker to limit loading of MSLIB namespace to required objects only. Separate out calculation of extractions from MsDataFileWorker to main-threaded ExtractionCalculator (to save re-calculation of same extraction multiple times, results of calculation are stored on first calcuation for preset number of uses); improve handling of retained spectral data as MsDataFileWorker advances through scans. Update QuantitationWorker to score spectra using weighted Euclidean rather (getSimilarityScoreAgainst) rather than normalised spectral contrast angle (undervalues high intensity isotopologues); exclude isotopologues used for quantifying lower-mass label from re-use in higher mass label (Config option _ExcludeLowerMassLabelMassesFromMatching); fix bug excluding scans at limits of RT window from apex matching; report Quant info in flat array of 4-index blocks (TotalInt,Inc,RT,Score); move extended quant info to be appended to Quant array rather than separate object.  Modify HeavyMetL.htm to account for new ExtractionCalculator and TaskQueue and both HeavyMetL.htm and HeavyMetL.css for display of progress info from Interface.  Update Config with better performing default tolerances, and add default settings for new options.

--- cfg/config.js
+++ cfg/config.js
@@ -1,15 +1,22 @@
 if (typeof HML == 'undefined') HML = {};
 
 HML.Config = {
+ //Basic Options (Visible in Settings)
  FixedMods : "Carbamidomethyl (C)", //assume these modifications are on all matching residues (mods are defined in Modifications.js) 
- PpmError : 10, //lowering this can increase performance (and quant quality if data is high resolution and good mass accuracy)
- RTSearchWindow  : 1, //lowering this increases performance but may result in missing the peak if chromatographic reproducibility is low
- RTMaxShift  : 0.1, //max distance between light and heavy peak apex
- RTOrder  : -1, //should the heavy peak elute before (-1) or after (1) the light peak.  0 is no constraint
+ PpmError : 5, //lowering this can increase performance (and quant quality if data is high resolution and good mass accuracy)
+ RTSearchWindow  : 0.5, //lowering this increases performance but may result in missing the peak if chromatographic reproducibility is low (mins)
+ RTMaxShift  : 0.05, //max distance between light and heavy peak apex (mins).  Zero means heavy peak will always be measured at light peak apex
+ RTOrder  : 0, //should the heavy peak elute before (-1) or after (1) the light peak.  Zero disables this constraint.
  IncorporationWindowMin : 10, //lowest % incorporation to look for
  IncorporationWindowMax : 100, //highest % incorporation to look for
+ DisplayChromatogramIntensitiesLogged : false, //whether to show the chromatogram intensities on a log10 scale
+ DisplayYAxisDecimalPlaces : 2,
+
+ //Advanced Options (Hidden from Settings)
+ _MatchScoreThreshold : 0.85, //Distribution of scores shows a fairly sharp elbow at 0.85
+ _ExcludeLowerMassLabelMassesFromMatching : true,
  _FindMaxIntensityByLowestIsotopologue : true, //locate peak apex using the lowest intensity isotopologue in each scan to calculate the intensity of the full distribution (makes algorithm more robust vs co-elution)
- _ConsiderIsotopologuesInTop : 0.8, //what proportion of the distribution to consider when locating peak apexes (setting to 1 will include low proportion isotopologues and will in most cases increase noise & decrease quant quality) 
- _QuantitationThreads : 8, //best set to the number of cores (allowing for hyperthreading).  Lower numbers underutilise resources and higher numbers will waste time with context switching.
- _RawFileMaxBlockProcessSize : Math.pow(2,27), //String length limit is (2^28)-1 in Firefox and (2^28)-(2^4) in Chrome.  N.B. Safari has a limit of (2^31)-1 but Safari is not a currently targeted browser.  If processing Thermo Raw Files (and therefore using ArrayBuffers not strings) this is potentially much larger.
+ _ConsiderIsotopologuesInTop : 0.7, //what proportion of the distribution to consider when locating peak apexes (setting to 1 will include low proportion isotopologues and will in most cases increase noise & decrease quant quality).  Also used to define the mass range of the unlabelled feature not considered in the labelled feature quantation step.
+ _QuantitationThreads : 8, //best set to the number of logical processors (allowing for hyperthreading).  Lower numbers underutilise resources and higher numbers will waste time with context switching.
+ _RawFileMaxBlockProcessSize : Math.pow(2,27), //String length limit is (2^28)-1 in Firefox and (2^28)-(2^4) in Chrome.  N.B. Safari has a limit of (2^31)-1 but Safari is not a currently targeted browser.  If processing Thermo Raw Files (and therefore using ArrayBuffers not strings) the memory limit is potentially much larger.  The default of 2^27 avoids some memory-related stability issues at higher numbers.
 }
