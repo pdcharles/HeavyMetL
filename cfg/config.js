2c1a69bffe06f2daeac7c90b82e3ece4b28f5882
Phil Charles <estofi.uk@gmail.com>
2020-03-13 09:57:59 +0000

    Bugfix page load event firing; update mslib name change (from uppercase); change UI plot labels from 'Incorporation Rate' to 'Incorporation Level'; update comments in config.js

--- cfg/config.js
+++ cfg/config.js
@@ -20,8 +20,8 @@ HML.config = {
  _findMaxIntensityByLowestIsotopologue : true, //locate peak apex using the lowest intensity isotopologue in each scan to calculate the intensity of the full distribution (makes algorithm more robust vs co-elution)
  _considerIsotopologuesInTop : 0.7, //what proportion of the distribution to consider when locating peak apexes (setting to 1 will include low proportion isotopologues and will in most cases increase noise & decrease quant quality).  Also used to define the mass range of the unlabelled feature not considered in the labelled feature quantation step.
  _quantitationThreads : 8, //best set to the number of logical processors (allowing for hyperthreading).  Lower numbers underutilise resources and higher numbers will waste time with context switching.
- _rawFileMaxBlockProcessSize : Math.pow(2,27), //String length limit is (2^28)-1 in Firefox and (2^28)-(2^4) in Chrome.  N.B. Safari has a limit of (2^31)-1 but Safari is not a currently targeted browser.  If processing Thermo Raw Files (and therefore using ArrayBuffers not strings) the memory limit is potentially much larger.  The default of 2^27 avoids some memory-related stability issues at higher values.
+ _rawFileMaxBlockProcessSize : Math.pow(2,28)-Math.pow(2,4), //String length limit is (2^28)-1 in Firefox and (2^28)-(2^4) in Chrome.  N.B. Safari has a limit of (2^31)-1 but Safari is not a currently targeted browser.  If processing Thermo Raw Files (and therefore using ArrayBuffers not strings) the memory limit is potentially much larger.  Setting value to 2^27 or lower may avoid some memory-related stability issues (though these seem to have been fixed in recent browser versions).
  //!!Experimental!!:
  _unlabelledIncorporationWindowMin : 0.368, //Equivalent of incorporationWindowMin for the unlabelled channel. Default 0.368
- _unlabelledIncorporationWindowMax : 0.368, //Equivalent of incorporationWindowMax for the unlabelled channel. Default 0.368
+ _unlabelledIncorporationWindowMax : 0.368 //Equivalent of incorporationWindowMax for the unlabelled channel. Default 0.368
 }
